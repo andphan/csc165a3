c53a3d82b1f99b1d10689946c891af282223797a
andphan <amannamedphan@gmail.com>
2015-03-24 00:37:17 -0700

    testing terrain.

--- src/a3/MyGame.java
+++ src/a3/MyGame.java
@@ -3,6 +3,7 @@ package a3;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import javax.imageio.ImageIO;
 
@@ -26,13 +27,14 @@ public class MyGame extends BaseGame {
 	private Group rootNode;
 	private SkyBox skybox;
 	private ICamera camera;
-	private TerrainBlock parkingLot;
+	private TerrainPage parkingLot;
 	private IDisplaySystem display;
+	private Texture tf;
 	private Group scene;
 	public static final String dir = "." + File.separator + "src" + File.separator + "a3"+ File.separator + "images" + File.separator; 
 	public static final String textureT = "heightMapTest.bmp";
 	public static final String textureB = "lotTest.bmp";
-	public static final String textureC = "testFloor.bmp";
+	public static final String textureC = "squaresquare.bmp";
 	
 	public static final String texDir = dir + textureT;
 	public static final String texxDir = dir + textureB;
@@ -62,34 +64,46 @@ public class MyGame extends BaseGame {
 	{
 		scene = new Group("Root Node");
 		
-		skybox = new SkyBox("SkyBox", 20.0f, 20.0f, 20.0f);
+	//	skybox = new SkyBox("SkyBox", 20.0f, 20.0f, 20.0f);
 		
 		Texture tex = TextureManager.loadTexture2D(texDir);
-
+		Texture texF = TextureManager.loadTexture2D(texxDir);
 		
-		
-		skybox.setTexture(SkyBox.Face.North, tex);
-		skybox.setTexture(SkyBox.Face.South, tex);
-		scene.addChild(skybox);
+	//	skybox.setTexture(SkyBox.Face.North, tex);
+	//	skybox.setTexture(SkyBox.Face.South, tex);
+	//	scene.addChild(skybox);
 		
 		Pyramid pyr = new Pyramid("Pyramid");
 		pyr.translate(5, 2, 2);
 		
-		
-		
+	
 		AbstractHeightMap heightmap = null;
 
-	//	Texture heightMapImage = TextureManager.loadTexture2D(texxxDir);		
-	//	heightmap.setHeightScale(0);
-	//	heightmap.setHeightAtPoint(100, 10, 10);
-	//	heightmap.setSize(25);
-    //	heightmap.load();
+		heightmap = new ImageBasedHeightMap(texF.getImage());
+		heightmap.load();
+
+		Vector3D scaleFactor = new Vector3D(new Point3D(1, 1, 1));
 		
+		try
+		{
+		parkingLot = new TerrainPage("terrainp", 200, heightmap.getSize(), scaleFactor, heightmap.getHeightData());
+	//	parkingLot = new TerrainBlock("tblock", 200, 513, heightmap.getHeightData());
+	//	parkingLot = new TerrainBlock("terrainname", 200, scaleFactor, heightmap.getHeightData(), new Point3D(0, 0, 0));
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		scene.addChild(parkingLot);
+		
+		/*
+		Exception in thread "AWT-EventQueue-0" java.util.ConcurrentModificationException
+	at java.util.ArrayList$Itr.checkForComodification(Unknown Source)
+		*/
 
 //		parkingLot = new TerrainBlock("Terrain", 65, new Vector3D(10, 10, 10), null, new Point3D(0, 0, 0));
 		
 	//    scene.addChild(parkingLot);
-		
+	
 		scene.addChild(pyr);
 		addGameWorldObject(scene);
 		
